417eaa0f9e0be74f84b136dbfaec683967deeaed
jakubr1712 <jakubr1712@gmail.com>
2024-02-11 13:14:03 +0100

    findMiddle, nthLastNode, SwapNodes

---
+++ SingleLinkedList/findMiddle.js
@@ -0,0 +1,15 @@
+const findMiddle = (linkedList) => {
+  let fast = linkedList.head;
+  let slow = linkedList.head;
+
+  while (fast !== null) {
+    fast = fast.getNextNode();
+    if (fast !== null) {
+      fast = fast.getNextNode();
+      slow = slow.getNextNode();
+    }
+  }
+  return slow;
+};
+
+module.exports = findMiddle;
---
+++ SingleLinkedList/nthLastNode.js
@@ -0,0 +1,19 @@
+const nthLastNode = (linkedList, n) => {
+  let current = null;
+  let tailSeeker = linkedList.head;
+  let count = 0;
+  while (tailSeeker) {
+    tailSeeker = tailSeeker.next;
+
+    if (count >= n) {
+      if (!current) {
+        current = linkedList.head;
+      }
+      current = current.next;
+    }
+    count++;
+  }
+  return current;
+};
+
+module.exports = nthLastNode;
